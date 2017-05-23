3c15a94f6dc6815a58c4a48b8f4a10b8c24a91fb
John Dowd and kimlan nguyen <john+kimlan@>
2017-05-23 11:03:49 -0700

    Add form to take in car's model, car's miles, car's price. Add .cshtml to display inventory, car added, clear car

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,3 +1,27 @@
 using Nancy;
 using System.Collections.Generic;
 using carDealership_2.Objects;
+
+namespace carDealership_2
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => View["index.cshtml"];
+      Get["/view_all_car"] = _ => {
+        List<cars> allCar = cars.ShowInventory();
+        return View["view_all_cars.cshtml", allCar];
+      };
+      Post["/cars_added"] = _ => {
+        cars newCar = new cars(Request.Form["model"], Request.Form["miles"], Request.Form["price"]);
+        newCar.Save();
+        return View["cars_added.cshtml", newCar];
+      };
+      Post["/car_cleared"] = _ => {
+        cars.ClearAll();
+        return View["car_cleared.cshtml"];
+      };
+    }
+  }
+}
--- Objects/cars.cs
+++ Objects/cars.cs
@@ -1,1 +1,86 @@
 using System.Collections.Generic;
+namespace carDealership_2.Objects
+{
+  public class cars
+  {
+    private string _model;
+    private int _price;
+    private int _miles;
+    private static List<cars> _instances = new List<cars>();
+    public cars(string makeModel, int miles, int price)
+    {
+    SetModel(makeModel);
+    SetPrice(price);
+    SetMiles(miles);
+    }
+
+    public void SetModel(string newModel)
+    {
+      _model = newModel;
+    }
+
+    public string GetModel()
+    {
+      return _model;
+    }
+
+    public void SetMiles(int newMiles)
+    {
+      _miles = newMiles;
+    }
+
+    public int GetMiles()
+    {
+      return _miles;
+    }
+
+    public void SetPrice(int newPrice)
+    {
+      _price = newPrice;
+    }
+
+    public int GetPrice()
+    {
+      return _price;
+    }
+
+    public static List<cars> ShowInventory()
+    {
+      return _instances;
+    }
+
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    // Car porsche = new Car("2014 Porsche 911", 114991, 786);
+    //
+    // Car ford = new Car("2011 Ford F450", 55995, 14241);
+    //
+    // Car lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+    //
+    // Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+    //
+    // private static List<Car> _instances = new List<Car>()  { porsche, ford, lexus, mercedes };
+    //
+    // public static List<Car> ShowInventory()
+    // {
+    //   foreach(Car automobile in _instances)
+    //   {
+    //     List<string> inventory = new List<string>();
+    //     inventory.Add(automobile.GetModel());
+    //   }
+    //   return inventory;
+    // }
+
+
+
+  }
+
+}
